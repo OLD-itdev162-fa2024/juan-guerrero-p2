792c3ce2e36ce14b75f8895f41053b023091dd54
Juan Guerrero <guerrjm1@gmatc.matc.edu>
2023-09-26 15:10:54 -0500

    Comapleted Activity 4

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +22,12 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
+
+app.UseCors(policy => policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+    
 app.UseAuthorization();
 
 app.MapControllers();
