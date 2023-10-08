2d0647c62e49e30dcc60c82a9df8c534df4495bf
Juan Guerrero <guerrjm1@gmatc.matc.edu>
2023-10-08 14:18:42 -0500

    Completed Acrivity 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,32 @@
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+//import { error } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
-  weatherForecasts: any;
 
-  constructor(private http: HttpClient){
+export class AppComponent implements OnInit {
+  title = 'client';
+  //weatherForecasts: any;
+  posts: any;
+
+  constructor(private http: HttpClient)
+  {
 
   }
 
-  ngOnInit(): void {
-    this.http.get('http://localhost:5164/weatherforecast').subscribe(
-      response => { this.weatherForecasts = response; },
-      error => { console.log(error) }
+  ngOnInit(): void
+  {
+    this.http.get('http://localhost:5164/api/posts').subscribe(
+      response => {this.posts = response; },
+      error => {console.log(error)}
     );
+    /*this.http.get('http://localhost:5037/weatherforecast').subscribe(
+      response => {this.weatherForecasts = response; },
+      error => {console.log(error)}
+    );*/
   }
 }
-
