b92f8d2510d648e9cfeec8ed8cce9c46becc3367
Juan Guerrero <guerrjm1@gmatc.matc.edu>
2023-10-15 20:21:52 -0500

    Completed Activity 6

--- API/Controllers/PostController.cs
+++ API/Controllers/PostController.cs
@@ -1,30 +1,109 @@
-using Domain;
-using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers
 {
+
     [ApiController]
     [Route("api/[controller]")]
+
     public class PostsController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly DataContext context;
 
-        //Constructor
         public PostsController(DataContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
-        //GET api/posts
+        // GET api/posts
+
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
         [HttpGet(Name = "GetPosts")]
         public ActionResult<List<Post>> Get()
         {
-            return this._context.Posts.ToList();
+            return this.context.Posts.ToList();
         }
+
+        /// <summary>
+        /// GET aoi/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}", Name = "GetBtId")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+            if(post is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error creating post");
+        }
+
+        /// <summary>
+        /// PUT api/post
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns>An updated post</returns>
+        [HttpPut(Name = "Update")]
+        public ActionResult<Post> Update([FromBody]Post request)
+        {
+            var post = context.Posts.Find(request.Id);
+            if(post == null)
+            {
+                throw new Exception("could not find post");
+            }
+
+            // Update the post properties witht the request values, if present.
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != DateTime.MinValue ? request.Date : post.Date;
+
+            var success = context.SaveChanges() > 0;
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error updating post");
+        }
+        
     }
 }
